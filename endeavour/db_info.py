88c79910907e3b392a66b31000312332cbb6991d
0sw4l <ioswxd@gmail.com>
2015-11-12 05:27:12 -0500

    agregado wsgi, settings y el archivo que maneja las bases de datos

---
+++ endeavour/db_info.py
@@ -0,0 +1,15 @@
+__author__ = 'osw4l'
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+local = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+
+#heroku = {'default': dj_database_url.config(default=os.environ["DATABASE_URL"])}
