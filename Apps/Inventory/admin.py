2692771633f218ac835d0dc861298dfcb3a591cb
0sw4l <ioswxd@gmail.com>
2015-11-13 13:59:06 -0500

    se registro el modelo producto en el admin

--- Apps/Inventory/admin.py
+++ Apps/Inventory/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from models import Categoria
+from models import Categoria, Producto
 # Register your models here.
 
 
 @admin.register(Categoria)
 class CategoriaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
     pass
