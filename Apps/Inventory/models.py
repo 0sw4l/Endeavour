cad768da59470e551385d16e3d3505b830788ac4
0sw4l <ioswxd@gmail.com>
2015-11-13 14:00:27 -0500

    se creo un modelo para el tipo del producto, un campo de imagen, y otro de tipo para el producto, este ultimo es una fk del modelo TipoProducto

--- Apps/Inventory/models.py
+++ Apps/Inventory/models.py
@@ -11,14 +11,25 @@ class Categoria(models.Model):
         return self.nombre
 
 
+class TipoProducto(models.Model):
+
+    nombre = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.nombre
+
+
 class Producto(models.Model):
 
     nombre = models.CharField(max_length=50)
     categoria = models.ForeignKey(Categoria)
+    tipo = models.ForeignKey(TipoProducto)
     cantidad = models.PositiveIntegerField()
     precio = models.FloatField()
     descripcion = models.TextField()
+    imagen = models.ImageField(upload_to="endeavour")
     usado = models.BooleanField(default=False)
     eliminado = models.BooleanField(default=False, editable=False)
+
 
 
