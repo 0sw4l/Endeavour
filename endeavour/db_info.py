b95bd9e9dca55d0ef45cec00d239f0bae63ae1aa
0sw4l <ioswxd@gmail.com>
2015-11-30 00:08:38 -0500

    data base url

--- endeavour/db_info.py
+++ endeavour/db_info.py
@@ -11,14 +11,9 @@ local = {
     }
 }
 
-
+import dj_database_url
 heroku = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd3v0ik2pnveagn',
-        'USER': 'vlmhdhezimwxrk',
-        'PASSWORD': 'uJwPWYl6AwWShvCZ7rrVIBr1Xv',
-        'HOST': 'ec2-107-21-221-59.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=os.environ["DATABASE_URL"]
+    )
 }
