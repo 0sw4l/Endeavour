d847be08de04eac36c108cf8f49928f0d0d5ec75
0sw4l <ioswxd@gmail.com>
2015-11-30 00:49:43 -0500

    changes

--- endeavour/db_info.py
+++ endeavour/db_info.py
@@ -11,15 +11,13 @@ local = {
     }
 }
 
-
-heroku =     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'dc54al2keh8qn7',
-            'USER': 'xzboirdjtlychz',
-            'PASSWORD': 'ODuvlajUXPFO99PH49xzknEQzy',
-            'HOST': 'ec2-54-83-202-218.compute-1.amazonaws.com',
-            'PORT': '5432',
-        }
+heroku = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dc54al2keh8qn7',
+        'USER': 'xzboirdjtlychz',
+        'PASSWORD': 'ODuvlajUXPFO99PH49xzknEQzy',
+        'HOST': 'ec2-54-83-202-218.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
-
+}
