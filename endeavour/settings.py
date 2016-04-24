a561c8aa6dcf4c77419b802f26b4299a924f981c
0sw4l <ioswxd@gmail.com>
2016-04-23 20:26:21 -0500

    ...

--- endeavour/settings.py
+++ endeavour/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from . import db_info
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -89,7 +90,7 @@ WSGI_APPLICATION = 'endeavour.wsgi.application'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ioswxd@gmail.com'
-EMAIL_HOST_PASSWORD = 'xfyyazmpqgffjcrr'
+EMAIL_HOST_PASSWORD = 'hdgvefpzcmqrlxnn'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
@@ -98,9 +99,7 @@ EMAIL_USE_TLS = True
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 
-from . import db_info
-
-DATABASES = db_info.heroku
+DATABASES = db_info.local
 
 
 # Internationalization
